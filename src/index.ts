12995a851bbaeced9d0bad4d6be7c3d06e859543
Marceli Szerszen <mszerszen.dev@gmail.com>
2026-05-25 10:26:08 +0200

    Set up project

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { connectDB } from './config/db';
+import dns from 'node:dns';
+
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running with TypeScript!');
-});
+// endpoints
 
 app.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
